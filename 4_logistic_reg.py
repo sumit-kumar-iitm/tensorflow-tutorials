d3a166d6070c54a72e9d144d8fab569bca56583e
Kumar, Sumit <Sumit_Kumar2@intuit.com>
2016-10-13 23:33:26 +0530

    logistic regression

--- 4_logistic_reg.py
+++ 4_logistic_reg.py
@@ -1,70 +1,63 @@
 import tensorflow as tf
 import numpy as np
-import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
-plt.ion()
 
-try:
-    from tqdm import tqdm
-except:
-    def tqdm(x, *args, **kwargs):
-        return x
-
-np.random.seed(0)
-
-#Load data
 data = np.load('data/data_logistics/data_with_labels.npz')
-train_data = data['arr_0']/255
+
+normalized_train_data = data['arr_0']/255.
 labels = data['arr_1']
 
-print(train_data[0])
-print labels[0]
-
-plt.figure(figsize=(6,6))
-f, plts = plt.subplots(5, sharex=True)
-
-c = 91
-for i in range(5):
-    plts[i].pcolor(train_data[c + i*558], cmap=plt.cm.gray_r)
-
-
-def to_onehot(labels, nclasses=5):
-    outlabels = np.zeros((len(labels), nclasses))
+def to_onehot(labels, classes=5):
+    outputlabels = np.zeros((len(labels), 5))
     for i, l in enumerate(labels):
-        outlabels[i, l] = 1
+        outputlabels[i, l] = 1
+    return outputlabels
 
-    return outlabels
+onehot_labels = to_onehot(labels, 5)
 
-one_hot_labels = to_onehot(labels, 5)
+indices = np.random.permutation(normalized_train_data.shape[0])
+valid_cnt = int(normalized_train_data.shape[0]*0.1)
+test_idx, train_idx = indices[:valid_cnt], indices[valid_cnt:]
 
-indices = np.random.permutation(train_data.shape[0])
+test, train = normalized_train_data[test_idx, :], normalized_train_data[train_idx, :]
+onehot_test_labels, onehot_train_labels = onehot_labels[test_idx, :], onehot_labels[train_idx, :]
 
-valid_cnt = int(train_data.shape[0] * 0.1)
-test_idx, training_idx = indices[:valid_cnt], indices[valid_cnt:]
+test_flattened = test.reshape([-1, 1296])
+train_flattened = train.reshape([-1, 1296])
 
-test, train = train_data[test_idx, :], train_data[training_idx, :]
+x = tf.placeholder('float', [None, 1296])
+y_ = tf.placeholder('float', [None, 5])
 
-onehot_test_labels, pnehot_train_labels = one_hot_labels[test_idx, :], one_hot_labels[training_idx, :]
-
-
-sess = tf.Session()
-
-#shape is 36*36
-x = tf.placeholder("float", [None, 1296])
-y_ = tf.placeholder("float", [None, 5])
-
-#Variables
 W = tf.Variable(tf.zeros([1296, 5]))
 b = tf.Variable(tf.zeros([5]))
 
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-#cost function
 cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y + 1e-50, y_))
 
-#Training
-train_step = tf.train.GradientDescentOptimizer(0.02).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)
 
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+
+#training
+sess = tf.Session()
 sess.run(tf.initialize_all_variables())
+
+epochs = 1000
+test_acc = np.zeros(epochs//10)
+train_acc = np.zeros(epochs//10)
+for i in range(epochs):
+    if i%10 == 0:
+        A = sess.run(accuracy, feed_dict={x: train_flattened, y_: onehot_train_labels})
+        train_acc[i//10] = A
+        A = sess.run(accuracy, feed_dict={x: test_flattened, y_: onehot_test_labels})
+        test_acc[i//10] = A
+    sess.run(train_step, feed_dict={x: train_flattened, y_: onehot_train_labels})
+
+print test_acc[-1]
+print train_acc[-1]
+
+
 
